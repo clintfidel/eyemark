1dcee924c71942b57cd9f26084b56d74629868a6
femchaps <74244857+femchaps@users.noreply.github.com>
2021-09-22 20:09:28 +0100

    Survey

---
+++ src/modules/Mda/pages/Dashboard/pages/Analytics/pages/Screen5/Tabs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+function Tabs() {
+    return (
+        <div className="tabs">
+            Questions
+        </div>
+    )
+}
+
+export default Tabs;
---
+++ src/modules/Mda/pages/Dashboard/pages/Analytics/pages/Screen5/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+import "./Tabs.js"
+
+function Questions() {
+    return (
+        <>
+            <Tabs />
+            
+        </>
+    )
+}
+
+export default Questions;
